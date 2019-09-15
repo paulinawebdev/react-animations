831607d64fceb5198d450a11f6c55abe3c15face
Paulina C <paulinachee.is@gmail.com>
2019-09-14 17:37:58 -0700

    build files

---
+++ build/precache-manifest.1f64c5d7bcf56cb9b93bda30a87d8060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d0725220ec47ca34549d3437d04ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d40b88caa609689f7e",
+    "url": "/static/css/main.e85b78ff.chunk.css"
+  },
+  {
+    "revision": "b3da270adceab92eccd0",
+    "url": "/static/js/2.b4414291.chunk.js"
+  },
+  {
+    "revision": "19d40b88caa609689f7e",
+    "url": "/static/js/main.246d2f11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ddd5afdfaa4b9c1e017f15aeaca27693",
+    "url": "/static/media/blizzard-logo.ddd5afdf.svg"
+  },
+  {
+    "revision": "737690f40d53ba9f94fa1b6b7b6d5b01",
+    "url": "/static/media/nexon-logo.737690f4.png"
+  }
+]);
